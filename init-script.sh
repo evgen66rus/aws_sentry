898ba16af8fcd62fa8bdf87cfbb235507c66fbff
Евгений Алешин <evgen.alyoshin@gmail.com>
2021-05-25 10:42:25 +0500

    replaced tee

--- init-script.sh
+++ init-script.sh
@@ -3,7 +3,7 @@ apt update
 apt install -y python3 python3-pip git docker docker.io
 service docker start
 git clone https://github.com/getsentry/onpremise.git
-curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` | tee /usr/local/bin/docker-compose > /dev/null
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 cp /onpremise/sentry/sentry.conf.example.py /onpremise/sentry/sentry.conf.py
